3e96dc21a4d132b0ed7b5a177f351f341917551c
hunglouis <83754071+hunglouis@users.noreply.github.com>
2026-05-21 17:02:00 +0700

    cap nhat 17:01 21/05/2026

--- src/app/page.js
+++ src/app/page.js
@@ -2,7 +2,7 @@
 import AudioPlayer from 'react-h5-audio-player';
 import 'react-h5-audio-player/lib/styles.css';
 import { ethers } from 'ethers';
-import { createClient } from '@supabase/supabase-client'
+import { createClient } from '@supabase/supabase-js';
 import axios from 'axios';
 import emailjs from '@emailjs/browser';
 import { useEffect, useState } from 'react';
